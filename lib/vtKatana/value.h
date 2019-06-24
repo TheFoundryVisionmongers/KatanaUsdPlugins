7e6e462243e96cc75bd6fb8a6015814ced9adbbf
Mark Final <mark.final@foundry.com>
2019-06-24 11:48:20 +0100

    Foundry vtKatana: Windows needs to export template specialisations by using appropriate markup in the specialisation declarations. This does duplicate the work in the .cpp file but needed for Windows

--- lib/vtKatana/value.h
+++ lib/vtKatana/value.h
@@ -27,12 +27,68 @@
 #include "pxr/pxr.h"
 #include "vtKatana/traits.h"
 
+#if defined(ARCH_OS_WINDOWS)
+#include "api.h"
+#endif
+
 PXR_NAMESPACE_OPEN_SCOPE
 
 /// Copy \p value to a katana attribute, minimizing intermediate copies
 /// when possible, but always copying at least once.
 template <typename T>
 typename VtKatana_GetKatanaAttrType<T>::type VtKatanaCopy(const T& value);
+
+#if defined(ARCH_OS_WINDOWS)
+#define VTKATANA_EXPORT_VALUE_COPY(T)                                       \
+    template VTKATANA_API typename VtKatana_GetKatanaAttrType<T>::type VtKatanaCopy<T>(  \
+        const T& value);
+
+// Integral types
+
+VTKATANA_EXPORT_VALUE_COPY(bool)
+VTKATANA_EXPORT_VALUE_COPY(char)
+VTKATANA_EXPORT_VALUE_COPY(unsigned char)
+VTKATANA_EXPORT_VALUE_COPY(short)
+VTKATANA_EXPORT_VALUE_COPY(unsigned short)
+VTKATANA_EXPORT_VALUE_COPY(int)
+VTKATANA_EXPORT_VALUE_COPY(unsigned int)
+VTKATANA_EXPORT_VALUE_COPY(uint64_t)
+VTKATANA_EXPORT_VALUE_COPY(int64_t)
+VTKATANA_EXPORT_VALUE_COPY(long)
+
+// Vec Types
+VTKATANA_EXPORT_VALUE_COPY(GfVec2i)
+VTKATANA_EXPORT_VALUE_COPY(GfVec2f)
+VTKATANA_EXPORT_VALUE_COPY(GfVec2h)
+VTKATANA_EXPORT_VALUE_COPY(GfVec2d)
+VTKATANA_EXPORT_VALUE_COPY(GfVec3i)
+VTKATANA_EXPORT_VALUE_COPY(GfVec3f)
+VTKATANA_EXPORT_VALUE_COPY(GfVec3h)
+VTKATANA_EXPORT_VALUE_COPY(GfVec3d)
+VTKATANA_EXPORT_VALUE_COPY(GfVec4i)
+VTKATANA_EXPORT_VALUE_COPY(GfVec4f)
+VTKATANA_EXPORT_VALUE_COPY(GfVec4h)
+VTKATANA_EXPORT_VALUE_COPY(GfVec4d)
+
+// Matrix Types
+VTKATANA_EXPORT_VALUE_COPY(GfMatrix3f)
+VTKATANA_EXPORT_VALUE_COPY(GfMatrix3d)
+VTKATANA_EXPORT_VALUE_COPY(GfMatrix4f)
+VTKATANA_EXPORT_VALUE_COPY(GfMatrix4d)
+
+// Floating point types
+VTKATANA_EXPORT_VALUE_COPY(float)
+VTKATANA_EXPORT_VALUE_COPY(double)
+VTKATANA_EXPORT_VALUE_COPY(GfHalf)
+
+// String types
+VTKATANA_EXPORT_VALUE_COPY(std::string)
+VTKATANA_EXPORT_VALUE_COPY(SdfAssetPath)
+VTKATANA_EXPORT_VALUE_COPY(SdfPath)
+VTKATANA_EXPORT_VALUE_COPY(TfToken)
+
+#undef VTKATANA_EXPORT_VALUE_COPY
+#endif // defined(ARCH_OS_WINDOWS)
 
 PXR_NAMESPACE_CLOSE_SCOPE
 
