21e316c6a613bd71ace9083d8b46b76c285653c3
james.pedlingham <james.pedlingham@foundry.com>
2019-10-09 11:33:16 +0100

    Removing duplicate copyright notices and adding missing notices

--- lib/vtKatana/bootstrap.cpp
+++ lib/vtKatana/bootstrap.cpp
@@ -4,14 +4,6 @@
 // these plug-ins, and they moved to
 // https://github.com/TheFoundryVisionmongers/KatanaUsdPlugins
 // under the same Modified Apache 2.0 license, as shown below.
-// These files began life as part of the main USD distribution
-// https://github.com/PixarAnimationStudios/USD.
-// In 2019, Foundry and Pixar agreed Foundry should maintain and curate
-// these plug-ins, and they moved to
-// https://github.com/TheFoundryVisionmongers/KatanaUsdPlugins
-// under the same Modified Apache 2.0 license, as shown below.
-
-
 //
 // Copyright 2016 Pixar
 //
