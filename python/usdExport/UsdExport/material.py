1295e074627b005f1e05ce12b5c11828cdba4c0f
James Pedlingham <james.pedlingham@foundry.com>
2020-06-25 10:14:18 +0100

    TP 438382 - Reverting change which added support for shader names ending in underscores if the last character was a digit.
    Summary: In addition to TP 438578, reverting the support on the UsdExport side of things too.

--- python/usdExport/UsdExport/material.py
+++ python/usdExport/UsdExport/material.py
@@ -72,8 +72,6 @@ def CreateEmptyShaders(stage, materialNodes, materialPath):
         shader = UsdShade.Shader.Define(stage, shaderPath)
         shaderIdAttr = materialNode.getChildByName("type")
         shaderId = str(shaderIdAttr.getValue())
-        if shaderId and shaderId[-1] == "_":
-            shaderId = shaderId[:-1]
         shader.SetShaderId(shaderId)
 
 
